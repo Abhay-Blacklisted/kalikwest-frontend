0997d450e599ac2aa82b044885283f2bc19a5eda
Sahil <blacklisted.web04@gmail.com>
2026-02-18 10:09:54 +0530

    Footer Changes

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -8,12 +8,13 @@ const Footer = () => {
       <Container className={styles.footer}>
         <Row className='align-items-center flex-column-reverse flex-lg-row'>
             <Col xs={12} lg={4} xxl={6}>
-            <span  className={`${styles.copyrightText} text-center text-lg-start d-block`}> Copyright <NavLink to="/" className="text-decoration-none"> @Kalikwest Agency </NavLink>  All Rights Reserved. </span>
+            <span  className={`${styles.copyrightText} text-center text-lg-start d-block`}> Copyright <NavLink to="/" className="text-decoration-none"> @Kalikwest </NavLink>  All Rights Reserved. </span>
             </Col>
           <Col xs={12} lg={8}  xxl={6} >
           <Nav className='justify-content-center'>
             <Nav.Link as={NavLink} to="/services" className={`${styles.navLink}`}>Services</Nav.Link>
-              <Nav.Link as={NavLink} to="/portfolio" className={`${styles.navLink}`}>Project</Nav.Link>
+              <Nav.Link as={NavLink} to="/portfolio" className={`${styles.navLink}`}>Projects</Nav.Link>
+               <Nav.Link as={NavLink} to="/about" className={`${styles.navLink}`}>About Us</Nav.Link>
              <Nav.Link as={NavLink} to="/contact-us"  className={`${styles.navLink}`}>Contact</Nav.Link>  
             <Nav.Link as={NavLink} to="privacy-policy"  className={`${styles.navLink}`}>Privacy Policy</Nav.Link>
             <Nav.Link as={NavLink} to="terms-&-conditions"  className={`${styles.navLink}`}>Terms & Conditions</Nav.Link>       
